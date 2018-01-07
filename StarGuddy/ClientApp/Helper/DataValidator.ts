40d1ccd99d6606a4b50ff5ce6843f33b5273ec57
Ranjeet Kumar <work.ranjeet@gmail.com>
2018-01-08 01:52:01 +0530

    modified

---
+++ StarGuddy/ClientApp/Helper/DataValidator.ts
@@ -0,0 +1,9 @@
+﻿import { Injectable } from "@angular/core";
+
+@Injectable()
+export class DataValidator {
+
+    IsValidObject(result: any): boolean {
+        return Object.keys(result).length > 0;
+    }
+}
