7ceeb5ab89fe1f5ef6e8008f6be2d48240dfa6c4
Ranjeet Kumar <work.ranjeet@gmail.com>
2018-07-25 13:26:55 +0530

    ZX

---
+++ Core/StarGuddy.Api.Models/Profile/DancingModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StarGuddy.Api.Models.Profile
+{
+    public class DancingModel
+    {
+        public Guid Id { get; set; }
+
+
+    }
+}
