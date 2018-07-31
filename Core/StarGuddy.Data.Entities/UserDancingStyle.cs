b80f7e085a41c9d94bf8e4ce59f6b5c36066a178
Ranjeet Kumar <work.ranjeet@gmail.com>
2018-07-31 16:17:58 +0530

    asd

---
+++ Core/StarGuddy.Data.Entities/UserDancingStyle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StarGuddy.Data.Entities
+{
+    public class UserDancingStyle
+    {
+        public Guid Id { get; set; }
+        public Guid UserDancingId { get; set; }
+        public Int64 DancingStyleId { get; set; }
+    }
+}
